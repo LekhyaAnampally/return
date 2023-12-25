8ad5eecd45323b8f3b7d4c5d5bfb8fe16944a2e0
SowmyaAmbure12 <sowmyaambure@gmail.com>
2023-12-25 22:24:11 +0530

    pp

---
+++ tu.py
@@ -0,0 +1,9 @@
+noun_tuple=("shruthi","sow","hghgu","ddw")
+verb_t=("is dancing","is singing","is eating","is sleeping")
+N=int(input("enter any number"))
+if N>=len(noun_tuple):
+    print("vhguh")
+else:
+    noun=noun_tuple[N]
+    verb=verb_t[N]
+    print(noun,verb)
